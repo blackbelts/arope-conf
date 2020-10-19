7d9ab9b7dc14934451a87ef04bd3081ce179cee5
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-19 17:50:27 +0200

    test

--- models/policy.py
+++ models/policy.py
@@ -32,9 +32,71 @@ class AropePolicy(models.Model):
     agent_code = fields.Char('Agent Code', copy=True,)
     introdagt = fields.Char('Introdagt', copy=True,)
 
+    def create_end_requset(self):
+        form = self.env.ref('arope-conf.request_form_view')
+
+        return {
+            'name': ('Request'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'policy.request',
+            # 'view_id': [(self.env.ref('smart_claim.tree_insurance_claim').id), 'tree'],
+            'views': [(form.id, 'form')],
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+
+            'context': {'default_type':'end','default_policy':self.policy_num,'default_product':self.product}
+
+        }
+    def create_renew_requset(self):
+        form = self.env.ref('arope-conf.request_form_view')
+        return {
+            'name': ('Request'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'policy.request',
+            # 'view_id': [(self.env.ref('smart_claim.tree_insurance_claim').id), 'tree'],
+            'views': [(form.id, 'form')],
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+
+            'context': {'default_type':'renew' ,'default_policy':self.policy_num,'default_product':self.product}
 
 
+        }
 
+
+class AropePolicyRequests(models.Model):
+    _name = "policy.request"
+    _rec_name='name'
+
+    @api.model
+    def create(self, vals):
+        serial_no = self.env['ir.sequence'].next_by_code('req')
+        if vals.get('type') =='end':
+           r_type='End'
+        elif vals.get('type') =='renew':
+           r_type='Renew'
+        # merge code and serial number
+        vals['name'] =  str(serial_no)+'/'+r_type
+
+        return super(AropePolicyRequests, self).create(vals)
+
+    name=fields.Char('Request')
+    policy=fields.Integer('Policy Num')
+    product=fields.Char('Policy Product')
+    state = fields.Selection([('pending', 'Pending'),
+                              ('submitted', 'Submitted'), ('issued', 'Issued')], 'State', default='pending')
+
+
+    type =fields.Selection([('end', 'Endorsement'),
+                                ('renew', 'Renwal')],string='Request Type')
+    end_reason= fields.Text(string='Endorsement Reason')
+
+    def submit(self):
+        self.state='submitted'
+    def issue(self):
+        self.state='submitted'
 
 
 
