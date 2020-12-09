7304c78d971e8f48d11c7b9cbcb7a743cda7acce
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-09 14:33:56 +0200

    update

--- models/partner.py
+++ models/partner.py
@@ -11,6 +11,7 @@ class inhertResUser(models.Model):
 
     agent_code = fields.Char(string='Agent Code')
     card_id = fields.Char(string='Broker Card')
+    reset_mail = fields.Char(string='Mail For Reset Pass')
     related_person = fields.Many2one('persons',string='Related Person')
 
 class InheritBrokers(models.Model):
