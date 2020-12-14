f9b3f7a34eb9f5627861302999825d94fce1cf1a
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-14 17:14:44 +0200

    test

--- models/arope_api.py
+++ models/arope_api.py
@@ -62,9 +62,10 @@ class AropeIMS(models.Model):
             # for rec in data['claim']:
             #     persons = self.env['claim.arope'].update(dict(rec))
             #     return rec
-            persons = self.env['claim.arope'].create({'product': 'MAR', 'lob': 'Motor', 'policy_no': 147231,
-             'claimNo': 921, 'claim_serNo': 1501781,
-              'policy_serno': 1880657, 'curr': 'EGP', 'pin': 83687})
+            persons = self.env['claim.arope'].create({'product': 'MAR', 'lob': 'Motor', 'policy_no': 147231, 'sub_journal': '11', 'claim_year': 2019,
+             'claimNo': 921, 'claim_serNo': 1501781, 'date_declared': '2019-12-08', 'date_occured': '2019-12-05',
+             'claim_status': '1', 'policy_serno': 1880657, 'curr': 'EGP', 'pin': 83687, 'claim_eval': '309700.000',
+             'fees_eval': '505.000', 'recoveryamt_eval': '0.000', 'claim_paid': '0.000', 'agent_code': False})
 
             c_ids=self.env['claim.arope'].search([])
             return len(c_ids.ids)
