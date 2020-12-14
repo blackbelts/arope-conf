778ca9fd48f69b9c81b61e893df26e2a7a85c53c
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-14 13:17:17 +0200

    test

--- models/arope_api.py
+++ models/arope_api.py
@@ -10,6 +10,26 @@ class AropeIMS(models.Model):
     def update_arope_data(self,data,insert_date):
         c_write=False
         b_write = False
+        p_write = False
+        claim_write = False
+        coll_write = False
+        lob_exist=False
+        product_exist = False
+
+        if data['lob']:
+            ids = self.env['insurance.line.business'].search([]).ids
+            if ids:
+                lob_exist = True
+            else:
+                p_ids = self.self.env['insurance.line.business'].create(data['lob'])
+        if data['product']:
+            ids=self.env['insurance.product'].search([]).ids
+            if ids:
+                product_exist=True
+            else:
+                p_ids=self.self.env['insurance.product'].create(data['product'])
+
+
         if data['customer']:
             persons=self.env['persons'].create(data['customer'])
             search_ids=self.env['persons'].search([('type','=','customer'),('create_date','<',insert_date)]).unlink()
@@ -21,8 +41,30 @@ class AropeIMS(models.Model):
                 [('type', '=', 'broker'), ('create_date', '<', insert_date)]).unlink()
             # self.env['persons'].unlink(search_ids)
             b_write=True
-        if c_write and b_write:
-            return True
+        if data['policy']:
+            persons = self.env['policy.arope'].create(data['policy'])
+            search_ids = self.env['policy.arope'].search(
+                [('create_date', '<', insert_date)]).unlink()
+            # self.env['persons'].unlink(search_ids)
+            p_write=True
+        if data['claim']:
+            persons = self.env['claim.arope'].create(data['claim'])
+            search_ids = self.env['claim.arope'].search(
+                [('create_date', '<', insert_date)]).unlink()
+            # self.env['persons'].unlink(search_ids)
+            calim_write=True
+        if data['coll']:
+            persons = self.env['collection.arope'].create(data['broker'])
+            search_ids = self.env['collection.arope'].search(
+                [('create_date', '<', insert_date)]).unlink()
+            # self.env['persons'].unlink(search_ids)
+            coll_write=True
+        return [product_exist,lob_exist]
+        # if c_write and b_write and p_write and claim_write and coll_write:
+        #     return True
+        # else:
+        #     return "Data Not write successfully"
+
 
 
 
