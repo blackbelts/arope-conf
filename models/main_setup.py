1fe2b5aceda23dff3f0056ec48b3565e09cd59e0
blackbelts-egypt <mabdelwahed120468@gmail.com>
2021-02-14 01:48:35 +0200

    update

--- models/main_setup.py
+++ models/main_setup.py
@@ -38,7 +38,7 @@ class Product(models.Model):
 
 
 
-    questionnaire_ids = fields.One2many('questionnaire.line.setup', 'product_id')
+    questionnaire_ids = fields.One2many('questionnaire.line.setup', 'prod_id')
     # survey_ids = fields.One2many('survey.line.setup', 'product_id')
     # final_application_ids = fields.One2many('final.application.setup', 'product_id')
     # offer_setup_ids = fields.One2many('offer.setup', 'product_id')
