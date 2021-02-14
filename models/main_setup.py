c74b315b6b2fb59e26cec553ebdd25b9b94b8806
blackbelts-egypt <mabdelwahed120468@gmail.com>
2021-02-14 02:18:07 +0200

    update

--- models/main_setup.py
+++ models/main_setup.py
@@ -71,18 +71,6 @@ class Notification(models.Model):
 
     other_comm = fields.Float('Other Commission')
 
-class QuestionnaireLineSetup(models.Model):
-
-    _name = 'questionnaire.line.setup'
-    _rec_name = 'question'
-    question = fields.Char('Question')
-    options = fields.Many2many('selection.options', sting="Selections")
-    desc = fields.Char('Description')
-    question_type = fields.Selection([('text', 'Text'), ('numerical', 'Numerical'), ('choose', 'Choose')],
-                                     'Question Type', default='text')
-
-    # product_id = fields.Many2one('insurance.product', ondelele='cascade', index=True)
-    sub_questionnaire_id = fields.Many2one('sub.questionnaire', ondelele='cascade', index=True)
 
 
 
