9700fee2fa4d5f36ce5d97450c29e51d27236adb
blackbelts-egypt <mabdelwahed120468@gmail.com>
2021-03-03 16:32:27 +0200

    es

--- models/arope_api.py
+++ models/arope_api.py
@@ -43,17 +43,17 @@ class AropeIMS(models.Model):
             # c_write=True
         if data['broker']:
             log_id.broker = True
-            # search_ids = self.env['persons'].search(
-            #     [('type', '=', 'broker')]).unlink()
-            # persons = self.env['persons'].create(data['broker'])
+            search_ids = self.env['persons'].search(
+                [('type', '=', 'broker')]).unlink()
+            persons = self.env['persons'].create(data['broker'])
 
             # self.env['persons'].unlink(search_ids)
             # b_write=True
         if data['policy']:
             log_id.write({'policy':True})
 
-            # search_ids = self.env['policy.arope'].search([]).unlink()
-            # persons = self.env['policy.arope'].create(data['policy'])
+            search_ids = self.env['policy.arope'].search([]).unlink()
+            persons = self.env['policy.arope'].create(data['policy'])
 
             # self.env['persons'].unlink(search_ids)
             # log_id.p_write=True
@@ -71,14 +71,14 @@ class AropeIMS(models.Model):
         if data['claim']:
             log_id.write({'claim':True})
 
-            # search_ids = self.env['claim.arope'].search([]).unlink()
-            # persons = self.env['claim.arope'].create(data['claim'])
+            search_ids = self.env['claim.arope'].search([]).unlink()
+            persons = self.env['claim.arope'].create(data['claim'])
             claim_write=True
         if data['coll']:
             log_id.write({'collection':True})
 
-            # search_ids = self.env['collection.arope'].search([]).unlink()
-            # persons = self.env['collection.arope'].create(data['coll'])
+            search_ids = self.env['collection.arope'].search([]).unlink()
+            persons = self.env['collection.arope'].create(data['coll'])
             # coll_write=True
         # self.env['arope.log'].create({'broker':b_write,
         #                               'customer':c_write,'policy':p_write,'claim':claim_write,
